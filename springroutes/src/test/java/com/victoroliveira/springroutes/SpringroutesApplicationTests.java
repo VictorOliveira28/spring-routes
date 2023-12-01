0780896436dd8e0e7027330eb745817e6ac44548
Victor Oliveira <victor.oliveira20@icloud.com>
2023-12-01 15:59:45 -0300

    Iniciando projeto

---
+++ springroutes/src/test/java/com/victoroliveira/springroutes/SpringroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoroliveira.springroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
